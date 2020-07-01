95c8159099ff5ec6d1cea61c61bb01e5deb75a5b
Janella Senica <janella.senica@gmail.com>
2020-06-30 23:40:40 -0500

    final version

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-var APIKey = "c4ba4f73e7697b2af968370b13c7b401";
-var queryUrl = "api.openweathermap.org/data/2.5/weather?" + "q=austin&appid=" + APIKey;
-var city = $("searchBar").val();
+var APIKey = "&appid=c4ba4f73e7697b2af968370b13c7b401";
 
 // once you type in city in search bar, click search button
-$("searchBtn").on("click", function (event) {
-    var value = $(this).siblings("#searchBar").val();
-    var wxFind = $(this).parent().attr("#searchBtn");
+$(".searchBtn").on("click", function (event) {
+    var searchBar = $(".searchBar").val();
+    var value = $(this).siblings(".searchBar").val();
+    // var wxFind = $(this).parent().attr(".searchBtn");
 
     // used to prevent an event's default behavior
     event.preventDefault();
 
+    var city = $(".searchBar").val();
+    getWeather(city);
+
     // save to local storage
-    localStorage.setItem(value, wxFind);
+    localStorage.setItem(".searchBar", value);
+
+    $(".searchBar").val(localStorage.getItem(".searchBar"));
+
 });
 
-$("cities").val(localStorage.getItem("cities"));
-    
-    // grab text from input box
-    city = $("#searchBar").val();
+// grab text from input box
+function getWeather(city) {
+    console.log(city);
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + APIKey;
 
-// call to the OpenWeatherMap API
+    // call to the OpenWeatherMap API
     $.ajax({
         url: queryUrl,
         method: "GET"
 
-    // restore the data that we retrieved inside of an object called response
+        // restore the data that we retrieved inside of an object called response
     }).then(function (response) {
-        
+
         $("wxInfo").text(JSON.stringify(response));
 
         console.log(response);
 
-       
-        // Transfer content to HTML
-        $(".city").html("<h1>") + response.name + " Weather Details</h1>";
+        // Main Section - Transfer content to HTML
+        $(".currentDay").text(moment().format('l'));
+
+        $(".city").text("City: ") + response.name + "Weather Details</div>";
         $(".wind").text("Wind Speed: " + response.wind.speed);
         $(".humidity").text("Humidity: " + response.main.humidity);
 
@@ -47,5 +53,29 @@ $("cities").val(localStorage.getItem("cities"));
         console.log("Wind Speed: " + response.wind.speed);
         console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + tempF);
-        
+
+    
+        // 5-Day Forecast - Transfer content to HTML
+        $(".date").text("Date: ") + response.date + "Weather Details</div>";
+
+        // Convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        $(".temp").text("Temperature (K) " + response.main.temp);
+        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+        $(".humidity").text("Humidity: " + response.main.humidity);
+
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + tempF);
+
+        $(".date1").text(moment().format('l'));
+        $(".date2").text(moment().add(1, 'days').format('l'));
+        $(".date3").text(moment().add(2, 'days').format('l'));
+        $(".date4").text(moment().add(3, 'days').format('l'));
+        $(".date5").text(moment().add(4, 'days').format('l'));
+
     });
+}
